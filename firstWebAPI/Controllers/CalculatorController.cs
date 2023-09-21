217ebd97633c3b8b135219e7d51b2bf924403a6f
Nimal Chakravarthy  Balasubramaniam <nimalchakravarthy.b@gavstech.com>
2023-09-21 12:07:53 +0530

    FirstWebAPI

---
+++ firstWebAPI/Controllers/CalculatorController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace firstWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        // api/calculator/add?x=10&y=5
+        [HttpGet("Calculator/add")]
+        public int Add(int x,int y)
+        {
+            return x + y;
+        }
+        [HttpGet("Calculator/sum")]
+        public int Sum(int x, int y)
+        {
+            return x + y+1000;
+        }
+        // api/calculator/sub?x=10&y=5
+        [HttpPost]
+        public int Sub(int x, int y)
+        {
+            return x - y;
+        }
+        // api/calculator/mul?x=10&y=5
+        [HttpPut]
+        public int Mul(int x, int y)
+        {
+            return x * y;
+        }
+        // api/calculator/div?x=10&y=5
+        [HttpDelete]
+        public int Div(int x, int y)
+        {
+            return x / y;
+        }
+
+    }
+}
