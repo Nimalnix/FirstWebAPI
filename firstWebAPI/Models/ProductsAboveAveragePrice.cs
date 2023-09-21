cfe6de2ca182889509db703b6e5b96125240d61b
Nimal Chakravarthy Balasubramaniam <nimalchakravarthy.b@gavstech.com>
2023-09-21 14:44:02 +0530

    Done

---
+++ firstWebAPI/Models/ProductsAboveAveragePrice.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace firstWebAPI.Models;
+
+public partial class ProductsAboveAveragePrice
+{
+    public string ProductName { get; set; } = null!;
+
+    public decimal? UnitPrice { get; set; }
+}
